3f85d028820429288f7575628d5febf0b8c71555
Vqd2111 <vuquangduy2111@gmail.com>
2026-05-21 00:56:20 +0700

    Update TreePractice.cpp

--- Tuan11/TreePractice.cpp
+++ Tuan11/TreePractice.cpp
@@ -2,21 +2,22 @@
 #include <string>
 using namespace std;
 
-// Step 3: Cai dat thuat toan duyet cay NLR, LNR, LRN
+// Step 4: Toi uu ham InOrder de tu dong them dau ngoac bieu thuc
 struct Node { string data; Node* left; Node* right; };
 Node* CreateNode(string val) { Node* p = new Node; p->data = val; p->left = p->right = NULL; return p; }
 
-void PreOrder(Node* root) {
-    if (root != NULL) { cout << root->data << " "; PreOrder(root->left); PreOrder(root->right); }
-}
 void InOrder(Node* root) {
-    if (root != NULL) { InOrder(root->left); cout << root->data << " "; InOrder(root->right); }
-}
-void PostOrder(Node* root) {
-    if (root != NULL) { PostOrder(root->left); PostOrder(root->right); cout << root->data << " "; }
+    if (root != NULL) {
+        bool op = (root->data == "+" || root->data == "-" || root->data == "*" || root->data == "/");
+        if (op) cout << "( ";
+        InOrder(root->left);
+        cout << root->data << " ";
+        InOrder(root->right);
+        if (op) cout << ") ";
+    }
 }
 
 int main() {
-    cout << "--- TRAVERSAL ALGORITHMS ADDED ---\n";
+    cout << "--- FIXED EXPRESSION INORDER FORMAT ---\n";
     return 0;
 }
