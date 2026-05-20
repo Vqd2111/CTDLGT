3b8119cfadb2511d4d5d45b30837b2b3d1a69c50
Vqd2111 <vuquangduy2111@gmail.com>
2026-05-21 00:55:00 +0700

    Implement 4 special binary trees using pointers

--- Tuan11/TreePractice.cpp
+++ Tuan11/TreePractice.cpp
@@ -2,22 +2,24 @@
 #include <string>
 using namespace std;
 
-// Cau truc Node cay nhi phan dung con tro
-struct Node {
-    string data; 
-    Node* left;
-    Node* right;
-};
+// Step 2: Them cac ham tao 4 cay dac biet
+struct Node { string data; Node* left; Node* right; };
+Node* CreateNode(string val) { Node* p = new Node; p->data = val; p->left = p->right = NULL; return p; }
 
-Node* CreateNode(string val) {
-    Node* p = new Node;
-    p->data = val;
-    p->left = NULL;
-    p->right = NULL;
-    return p;
+Node* BuildFull() {
+    Node* t = CreateNode("A"); t->left = CreateNode("B"); t->right = CreateNode("C"); return t;
+}
+Node* BuildLeft() {
+    Node* t = CreateNode("A"); t->left = CreateNode("B"); return t;
+}
+Node* BuildRight() {
+    Node* t = CreateNode("A"); t->right = CreateNode("B"); return t;
+}
+Node* BuildZig() {
+    Node* t = CreateNode("A"); t->left = CreateNode("B"); t->left->right = CreateNode("C"); return t;
 }
 
 int main() {
-    cout << "=== TREE PRACTICE ===\n";
+    cout << "--- LINKED LIST TREE BUILD SUCCESS ---\n";
     return 0;
 }
