6983e5c6020be5cddbfd6c45540c0d7b04b34474
Vqd2111 <vuquangduy2111@gmail.com>
2026-05-27 23:40:50 +0700

    Update Testcase

--- Tuan12/HeapSort.cpp
+++ Tuan12/HeapSort.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 void printArray(const vector<int>& arr) {
-    cout << "[ ";
-    for (int i = 0; i < arr.size(); i++) cout << arr[i] << " ";
-    cout << "]" << endl;
+    cout << "[ "; for (int i = 0; i < arr.size(); i++) cout << arr[i] << " "; cout << "]" << endl;
 }
-
 void heapify(vector<int>& arr, int n, int i) {
     int largest = i; int left = 2 * i + 1; int right = 2 * i + 2;
     if (left < n && arr[left] > arr[largest]) largest = left;
     if (right < n && arr[right] > arr[largest]) largest = right;
     if (largest != i) { swap(arr[i], arr[largest]); heapify(arr, n, largest); }
 }
-
-// Bo sung log theo dung yeu cau barem
 void heapSort(vector<int>& arr) {
     int n = arr.size();
     cout << "Tao dong ban dau" << endl;
     for (int i = n / 2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-        cout << "Sau khi vun dong tai node" << i << ": ";
-        printArray(arr);
+        heapify(arr, n, i); cout << "Sau khi vun dong tai node" << i << ": "; printArray(arr);
     }
     cout << "Sap xep vun dong " << endl;
     for (int i = n - 1; i > 0; i--) {
-        swap(arr[0], arr[i]);
-        cout << "Hoan vi nut goc voi mang dang xet[" << i << "]: ";
-        printArray(arr);
-        heapify(arr, i, 0);
-        cout << "Vun dong lai nut goc: ";
-        printArray(arr);
+        swap(arr[0], arr[i]); cout << "Hoan vi nut goc voi mang dang xet[" << i << "]: "; printArray(arr);
+        heapify(arr, i, 0); cout << "Vun dong lai nut goc: "; printArray(arr);
     }
 }
 
-int main() { return 0; }
+int main() {
+    vector<int> arrSlide = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+    cout << "Cay trong slide" << endl;
+    cout << "Mang ban dau: "; printArray(arrSlide);
+    heapSort(arrSlide);
+    return 0;
+}
