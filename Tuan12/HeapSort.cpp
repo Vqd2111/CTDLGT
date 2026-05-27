5196de5dd6d13d7bcdec8061064c5c8faeb68c3f
Vqd2111 <vuquangduy2111@gmail.com>
2026-05-27 23:39:23 +0700

    Update HeapSort.cpp

--- Tuan12/HeapSort.cpp
+++ Tuan12/HeapSort.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-// Ham in mang sau moi thao tac
 void printArray(const vector<int>& arr) {
     cout << "[ ";
     for (int i = 0; i < arr.size(); i++) {
@@ -12,7 +11,27 @@ void printArray(const vector<int>& arr) {
     cout << "]" << endl;
 }
 
+// Ham vun dong tai mot node
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        // Vun dong lai de quy
+        heapify(arr, n, largest);
+    }
+}
+
 int main() {
-    cout << "--- KHOI TAO DU AN HEAP SORT ---\n";
+    cout << "--- THEM HAM HEAPIFY ---\n";
     return 0;
 }
