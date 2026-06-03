df09d8596e5addf5daebb8ce4db60c0421383cc3
Vqd2111 <vuquangduy2111@gmail.com>
2026-06-03 23:16:15 +0700

    Update BST_Search.cpp

--- Tuan13/BST_Search.cpp
+++ Tuan13/BST_Search.cpp
@@ -3,23 +3,21 @@
 
 using namespace std;
 
-// Cau truc Node cho Cay nhi phan tim kiem (BST)
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-};
+struct Node { int data; Node* left; Node* right; };
+Node* createNode(int value) { Node* newNode = new Node(); newNode->data = value; newNode->left = NULL; newNode->right = NULL; return newNode; }
 
-// Ham khoi tao Node moi
-Node* createNode(int value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+// Ham them Node vao BST (Quy tac: Trai <= Cha, Phai > Cha)
+Node* insertNode(Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+    
+    if (value <= root->data) {
+        root->left = insertNode(root->left, value);
+    } else {
+        root->right = insertNode(root->right, value);
+    }
+    return root;
 }
 
-int main() {
-    cout << "--- KHOI TAO DU AN CAY NHI PHAN TIM KIEM (BST) ---\n";
-    return 0;
-}
+int main() { return 0; }
