c7a91ec10bb9c3f57c090df3c4cc8686c9f7c1b1
Vqd2111 <vuquangduy2111@gmail.com>
2026-06-03 23:16:37 +0700

    Update BST_Search.cpp

--- Tuan13/BST_Search.cpp
+++ Tuan13/BST_Search.cpp
@@ -6,18 +6,20 @@ using namespace std;
 struct Node { int data; Node* left; Node* right; };
 Node* createNode(int value) { Node* newNode = new Node(); newNode->data = value; newNode->left = NULL; newNode->right = NULL; return newNode; }
 
-// Ham them Node vao BST (Quy tac: Trai <= Cha, Phai > Cha)
 Node* insertNode(Node* root, int value) {
-    if (root == NULL) {
-        return createNode(value);
-    }
-    
-    if (value <= root->data) {
-        root->left = insertNode(root->left, value);
-    } else {
-        root->right = insertNode(root->right, value);
-    }
+    if (root == NULL) return createNode(value);
+    if (value <= root->data) root->left = insertNode(root->left, value);
+    else root->right = insertNode(root->right, value);
     return root;
 }
 
-int main() { return 0; }
+int main() {
+    Node* root = NULL;
+    vector<int> years = {2001, 2002, 2006, 2007, 2003, 2004, 2005, 2001, 1999, 2004};
+    
+    for (int y : years) {
+        root = insertNode(root, y);
+    }
+    cout << "--- DA NAP DU LIEU NAM SINH VAO CAY ---\n";
+    return 0;
+}
