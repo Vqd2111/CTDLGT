219bfa75d6bb08e20a1d814e7d5f4de41e2291a8
Vqd2111 <vuquangduy2111@gmail.com>
2026-06-10 23:00:17 +0700

    L-R rotation

---
+++ Tuan14/AVLTree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Node { int data; Node* left; Node* right; int height; };
+int getHeight(Node* node) { if (node == NULL) return 0; return node->height; }
+
+// Phep quay phai (LL Case)
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    return x;
+}
+
+// Phep quay trai (RR Case)
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    return y;
+}
+
+int main() { return 0; }
