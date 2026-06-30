6b966f4c05913f7caa8b93f9da1e0ff1a977b05b
Vqd2111 <vuquangduy2111@gmail.com>
2026-06-30 09:34:14 +0700

    thuat toan DIJKSTRA

--- Tuan18/PRIM_KRUSKAL.cpp
+++ Tuan18/PRIM_KRUSKAL.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -17,9 +19,7 @@ struct Matran {
 void KhoiTaoMaTran(Matran &g, int soDinh) {
     g.n = soDinh;
     for (int i = 0; i < soDinh; i++) {
-        for (int j = 0; j < soDinh; j++) {
-            g.matrix[i][j] = 0;
-        }
+        for (int j = 0; j < soDinh; j++) g.matrix[i][j] = 0;
     }
 }
 
@@ -44,11 +44,43 @@ void InMaTran(int P[SO_TINH][SO_TINH], int n) {
     }
 }
 
+// Thuat toan Dijkstra tim chi phi di lai thap nhat tu mot dinh
+void ThuatToanDijkstra(Matran g, int start) {
+    int n = g.n;
+    vector<int> dist(n, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    dist[start] = 0;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (int v = 0; v < n; v++) {
+            if (g.matrix[u][v] > 0) {
+                if (dist[u] + g.matrix[u][v] < dist[v]) {
+                    dist[v] = dist[u] + g.matrix[u][v];
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+    }
+
+    cout << "\n=== DIJKSTRA: DUONG DI NGAN NHAT TU " << DSTinh[start] << " ===\n";
+    for (int i = 0; i < n; i++) {
+        if (i != start && dist[i] != INF)
+            cout << "Den " << DSTinh[i] << ": Trong so = " << dist[i] << "\n";
+    }
+}
+
 int main() {
     Matran g;
     KhoiTaoMaTran(g, SO_TINH);
 
-    // Du lieu trong so tuong ung voi chi so duong D1-D13 tren ban do
     int dsTuyenDuong[][3] = {
         {0, 6, 1}, {6, 7, 2}, {8, 7, 3}, {0, 8, 4}, {0, 9, 5},
         {0, 10, 6}, {0, 1, 7}, {0, 2, 8}, {2, 3, 9}, {3, 4, 10},
@@ -62,6 +94,8 @@ int main() {
 
     cout << "=== MA TRAN TRONG SO BAN DAU ===\n\n";
     InMaTran(g.matrix, SO_TINH);
+
+    ThuatToanDijkstra(g, 0);
 
     return 0;
 }
